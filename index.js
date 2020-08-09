28503b932eb16e7e660c6fbed19c3d2ed65289e6
xnacly <boomh0815@gmail.com>
2020-08-09 14:32:40 +0200

    init

---
+++ index.js
@@ -0,0 +1,36 @@
+document.getElementById("strings").disabled = true;
+let strings = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let numbers = "0123456789";
+let symbols = "!@#$%^&*()_+~|}{[]:;?><,./-=";
+let charset = strings;
+let flagNUM = false;
+let flagSym = false;
+
+function addNumbers() {
+	if (flagNUM) {
+		flagNUM = false;
+		return (charset = strings);
+	} else {
+		flagNUM = true;
+		return (charset += numbers);
+	}
+}
+
+function addSymbols() {
+	if (flagSym) {
+		flagSym = false;
+		return (charset = strings);
+	} else {
+		flagSym = true;
+		return (charset += symbols);
+	}
+}
+
+function genpw() {
+	let length = document.getElementById("length").value;
+	let retVal = "";
+	for (var i = 0, n = charset.length; i < length; ++i) {
+		retVal += charset.charAt(Math.floor(Math.random() * n));
+	}
+	return (document.getElementById("output").value = retVal);
+}
